9b8a2889ea8341c08a32cf77dae061a1ccc7a1b2
Davis Peck <Dpeck1997@gmail.com>
2022-03-01 12:56:42 -0700

    setting a list of hashes for fruit

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+item_1 = {"fruit" => "apple", "price" => "1.25", "type" => "fiji"}
+
+item_2 = {"fruit" => "orange", "price" => "1.45", "type" => "mandarin"}
+
+p "an #{item_1['fruit']} will cost about $#{item_1['price']}"
