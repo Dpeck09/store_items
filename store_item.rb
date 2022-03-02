0788d38705e98ed4c1419068da0e66becc291340
Davis Peck <Dpeck1997@gmail.com>
2022-03-02 12:24:12 -0700

    working with classes and making it DRY

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,59 @@
-item_1 = {"fruit" => "apple", "price" => "1.25", "type" => "fiji"}
+class StoreItem
+    attr_reader :item, :price, :availability
+    attr_writer :price, :availability, :item
 
-item_2 = {"fruit" => "orange", "price" => "1.45", "type" => "mandarin"}
+    # input_options = {
+    #     item: "bike", price: "325", availability: "here" 
+    # }
 
-p "an #{item_1['fruit']} will cost about $#{item_1['price']}"
+    def initialize(input_options)
+        @item = input_options[:item]
+        @price = input_options[:price]
+        @availability = input_options[:availability]
+    end
+
+    def print_info
+    p "our #{item} is #{availability} in store for the price of $#{price}."
+    end
+
+    # def availability=(input_availability)
+    #     @availability = input_availability
+    # end
+
+end
+
+
+item1 = StoreItem.new({
+    item: "bike", 
+    price: "325", 
+    availability: "here"
+})
+item2 = StoreItem.new({
+    item: "tv", 
+    price: "700", 
+    availability: "here"
+})
+
+# item1.print_info
+# item1.availability = "not here"
+# item1.print_info
+
+# item2.print_info
+
+class Manager < StoreItem
+    def item_report
+        p "here is what someone is looking for"
+       
+        p "person is ready to buy"
+    end
+end
+
+manager = Manager.new({
+    item: "bike",
+    price: "325",
+    availability: "here"
+})
+
+manager.item_report
+manager.print_info
+
